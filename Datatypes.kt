a463b3f772b28889cd343e3abd9defdd4e585278
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2024-04-08 14:13:47 +0530

    Datatypes In Kotlin

---
+++ Datatypes.kt
@@ -0,0 +1,21 @@
+//Datatypes
+/*
+Integer(Byte, Int, Short)
+Floating(Float, Double)
+Boolean(True, False)
+Character(Char, String)
+*/
+fun main() {
+    var kotlin : Int = 12;
+    var temp : Double = 22.5;
+    var israining : Boolean = false;
+    var namecharacter : Char = 'S';
+    var name : String = "Shruti";
+    
+    println(name);
+    println(namecharacter);
+    println(kotlin);
+    println(temp);
+    println(israining);
+    
+}
