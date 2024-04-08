c15e662d580d86c4b1c3bf9ef95667c016b70c91
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2024-04-08 14:02:09 +0530

    Variables In Kotlin

---
+++ Variables.kt
@@ -0,0 +1,16 @@
+//var and val variables in kotlin
+//var can be reassigned, val can't be reassigned
+fun main() {
+    var kotlin = "🙂"
+    println(kotlin)
+    kotlin="rt"
+    println(kotlin)
+    
+    val ko= 3;
+    println(ko)
+}
+
+/*output: 
+🙂
+rt
+3 */
