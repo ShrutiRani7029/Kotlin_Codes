6847be3fb495581cc3c6d4c0017a50644f1ab619
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2024-06-18 11:11:09 +0530

    Create Hello_World.kt
    #companion object: There is no static keyword in Kotlin, that's why we use companion object. it's similar to static members in other programming languages but with some added functionality.
    
    #JvmStatic annotations: If we use this annotation, the compiler will generate both a static method in the enclosing class of the object and an instance method in the object itself. 
    @JvmStatic applies to functions defined in companion objects of interfaces as well. Such functions compile to static methods in interfaces.

---
+++ Hello_World.kt
@@ -0,0 +1,11 @@
+package com.example.fundamentalskotlin
+
+class MyClass {
+    companion object{
+        @JvmStatic
+
+        fun main(args: Array<String>){
+            println("Hello world");
+        }
+    }
+}
