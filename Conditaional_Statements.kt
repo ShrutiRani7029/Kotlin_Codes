42c73f049f1337919eb561695edaa913bbb78216
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2024-06-18 14:24:49 +0530

    Create Conditaional_Statements.kt

---
+++ Conditaional_Statements.kt
@@ -0,0 +1,15 @@
+1)if
+2)else
+3)else-if
+4)when (replacement of switch)
+
+var num=16
+if(num>100) println("greater")
+else if(num==100)println("equal")
+else println("smaller")
+
+when{
+  num>100 -> { println("num is greater")
+  num<100->  {println("num is smaller") }
+
+  
